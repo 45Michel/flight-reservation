bd97426c81d35dd0a693f31cd137c155847a5cb5
Michel L <mmarulanda785@gmail.com>
2025-10-13 16:42:59 -0500

    danielaaaa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path: 'vuelos',
+    loadComponent: () => import('../app/pages/vuelos/vuelos.component').then((m) => m.VuelosComponent)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('../app/pages/register/register.component').then((m) => m.RegisterComponent)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('../app/pages/login/login.component').then((m) => m.LoginComponent)
+  }
+
+];
---
+++ src/app/pages/vuelos/vuelos.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-vuelos',
+  imports: [
+    ButtonModule
+  ],
+  templateUrl: './vuelos.component.html',
+  styleUrl: './vuelos.component.scss'
+})
+export class VuelosComponent {
+
+}
